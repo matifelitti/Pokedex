781e23885c7588c81873be43ec70fb4bd5406807
matifelitti <matiasfelitti@gmail.com>
2024-05-09 20:09:46 -0300

    fetching all  pokemon

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
-const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+const color = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
 
-const color = [
-    0 = 'black',
-    1 = 'blue',
-    2 = 'brown',
-    3 = 'grey',
-    4 = 'green',
-    5 = 'pink',
-    6 = 'purple',
-    7 = 'red',
-    8 = 'white',
-    9 = 'yellow',
-];
+const fetchAllPokemon = async () => {
+  for (let i = 1; i <= 151; i++) {
+    await pokemon(i);
+  }
+};
+
+const pokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  pokemonCard(data);
+};
+
+pokemonCard = (pokemon) => {
+  const container = document.querySelector(".container");
+  const div = document.createElement("div");
+  div.classList.add("pokelist");
+  const card = `<div>
+  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"></img>
+  <h2>${pokemon.name}</h2>
+  </div>
+  `;
+  div.innerHTML = card;
+  container.append(div);
+};
+
+fetchAllPokemon();
