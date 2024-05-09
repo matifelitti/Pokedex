7798c88b5e2aa3c12100e9e4e069959a2133aaed
matifelitti <matiasfelitti@gmail.com>
2024-05-08 23:01:34 -0300

    adding script url color - adding div html

---
+++ script.js
@@ -0,0 +1,14 @@
+const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+const color = [
+    0 = 'black',
+    1 = 'blue',
+    2 = 'brown',
+    3 = 'grey',
+    4 = 'green',
+    5 = 'pink',
+    6 = 'purple',
+    7 = 'red',
+    8 = 'white',
+    9 = 'yellow',
+];
